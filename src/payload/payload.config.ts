2fc9743b10c3ddff9d8257b5133fc0f1640975b4
Daniel Lewis <dan.lewis@bamboocreative.nz>
2023-10-26 16:54:41 +0200

    add base config to get payload running with pg

--- src/payload/payload.config.ts
+++ src/payload/payload.config.ts
@@ -1,14 +1,36 @@
 import path from 'path';
 import { buildConfig } from 'payload/config';
+import { webpackBundler } from '@payloadcms/bundler-webpack';
+import { postgresAdapter } from '@payloadcms/db-postgres';
+import { lexicalEditor } from '@payloadcms/richtext-lexical';
+import search from '@payloadcms/plugin-search';
 
 export default buildConfig({
-  collections: [
-    // Your collections here
-  ],
-  globals: [
-    // Your globals here
-  ],
-  typescript: {
-    outputFile: path.resolve(__dirname, '../payload-types.ts'),
-  },
+	db: postgresAdapter({
+		pool: {
+			connectionString: process.env.POSTGRES_URI
+		}
+	}),
+	//@ts-ignore
+	editor: lexicalEditor({}),
+	admin: {
+		bundler: webpackBundler()
+	},
+	plugins: [
+		search({
+			defaultPriorities: {
+				pages: ({ doc }) => (doc.title.startsWith('Hello, world!') ? 1 : 10),
+				posts: 20
+			}
+		})
+	],
+	collections: [
+		// Your collections here
+	],
+	globals: [
+		// Your globals here
+	],
+	typescript: {
+		outputFile: path.resolve(__dirname, '../payload-types.ts')
+	}
 });
